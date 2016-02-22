76b55d83a1f9da5497fb83a7304ff0f0b12816be
Valter Lima Mesquita <limavalter@gmail.com>
2016-02-22 17:40:44 -0300

    Displaying results

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var imagesLib = require('../lib/image-search.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/latest', function(req, res, next){
+
+  var history = imagesLib.getHistory();
+  res.send(history);
+
+})
+
+router.get('/:search', function(req, res, next) {
+
+  var resource = imagesLib.search(req.params.search, req.query, function(err, result){
+    res.send(result);
+  });
+
+});
+
 
 module.exports = router;
